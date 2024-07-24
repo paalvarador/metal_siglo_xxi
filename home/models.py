9ec255c23921787cc8a770ca129d9531569caa10
Pablo Alvarado <paalvarador@gmail.com>
2024-07-23 20:13:59 -0500

    feat: Implementing Model Banner

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Model to manage de Sliders show in the HomePage (Swiper)
+class Banner(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(upload_to='banners/')
+    link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/')
@@ -16,3 +16,4 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return f"{self.product.name} - {'Primary' if self.is_primary else 'Secondary'}"
+
