3857d4dfe94f5fa425053d475469596dcc697397
Pablo Alvarado <paalvarador@gmail.com>
2024-08-06 23:07:18 -0500

    feat: Algunos cambios en las urls del servicio

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import BannerCreate, BannerUpdate, BannerList, BannerDetail, BannerDelete, ContactInfoCreate, ContactInfoUpdate, ContactInfoList, ContactInfoDetail, TestimonialCreate, TestimonialUpdate, TestimonialList, TestimonialDetail, CompanyInfoCreate, CompanyInfoUpdate, CompanyInfoList, CompanyInfoDetail
 
 urlpatterns = [
-    path('banners', BannerCreate.as_view(), name='banner_create'),
-    path('banners/<int:pk>', BannerUpdate.as_view(), name='banner_update'),
-    path('banners', BannerList.as_view(), name='banner_list'),
-    path('banners/<int:pk>', BannerDetail.as_view(), name='banner_detail'),
-    path('banner/delete/<int:pk>', BannerDelete.as_view(), name='banner_delete'),
+    path('home', BannerCreate.as_view(), name='banner_create'),
+    path('home/<int:pk>', BannerUpdate.as_view(), name='banner_update'),
+    path('home', BannerList.as_view(), name='banner_list'),
+    path('home/<int:pk>', BannerDetail.as_view(), name='banner_detail'),
+    path('home/delete/<int:pk>', BannerDelete.as_view(), name='banner_delete'),
 ]
